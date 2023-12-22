13411366f6f9efc07fdf242186426bf094b9ac12
mockingjay <8400684@qq.com>
2023-12-22 17:23:31 +0800

    ini

--- ControlCatalogStandalone.Desktop/Program.cs
+++ ControlCatalogStandalone.Desktop/Program.cs
@@ -17,7 +17,7 @@ class Program
     {
         var app = AppBuilder.Configure<App>()
             .UseSkia() // skia
-            .UseFontSourceSansPro()
+            .UseFontSourceSansPro()//In some platforms, it may not be opened after publishing with aot, please remove it!
             .LogToTrace();
 
 #if Linux
